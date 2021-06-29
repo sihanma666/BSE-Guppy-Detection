414f928b73f92cd77a66f6415b2594a3c3d11b8f
Sihan Ma <sihanma@gmail.com>
2021-06-29 18:19:55 -0500

    error in json.load()

--- fancylivestream.py
+++ fancylivestream.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import requests
 import time
+import json
 
 cap = cv2.VideoCapture(0)
 url = 'https://app.nanonets.com/api/v2/ObjectDetection/Model/46b66638-6bdf-49ba-b600-9e83a8dfe9b7/LabelFile/'
@@ -19,17 +20,16 @@ while True:
     response = requests.post(url, auth=requests.auth.HTTPBasicAuth('VuQnpnWewkvGPzKMmv0EM_CgE6zElUmc', ''), files=data)
     print('API called')
     
-    short = d[result][0]['predictions'][0]
-    score = short['score']
-    xmax = short['xmax']
-    xmin = short['xmin']
-    ymax = short['ymax']
-    ymin = short['ymin']
+    d = json.loads(response.text)
     
-    if(score<0.8){
-        d[result][0]['predictions'][0]['label'] = ""
-        response = requests.reques('POST', url, auth=requests.auth.HTTPBasicAuth('VuQnpnWewkvGPzKMmv0EM_CgE6zElUmc', ''))
+    score = d['result'][0]['predictions'][0]['score']
+    xmax = d['result'][0]['predictions'][0]['xmax']
+    xmin = d['result'][0]['predictions'][0]['xmin']
+    ymax = d['result'][0]['predictions'][0]['ymax']
+    ymin = d['result'][0]['predictions'][0]['ymin']
     
+    #if score<0.8:
+        
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord(*'g'):
